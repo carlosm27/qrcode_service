c27b5fdd961dea36919cb8e510c56ceed18e7a35
Carlos Armando Marcano Vargas <c_marcano@correo.unimet.edu.ve>
2022-09-21 16:29:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 if __name__ == "__main__":
 
     import uvicorn
+    import os
+    from dotenv import load_dotenv
 
-    uvicorn.run("app:app", reload=True)
+    uvicorn.run("app:app", host=HOST, port=PORT)
